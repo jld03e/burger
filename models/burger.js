11fb7d86025ef2d7973f8cba2b73f961090a6c68
Jose De Las Salas <jld03e@gmail.com>
2018-08-20 10:12:08 -0400

    added files and some functionaility to site pending the inclusion of more functionality.

--- models/burger.js
+++ models/burger.js
@@ -6,6 +6,16 @@ var burger = {
         orm.selectAll("burgers", function(res){
             cb(res);
         });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+        });
     }
 };
 
