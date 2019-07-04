a9f660f59f57feaa32d7180e609371e73617e6ce
Jose De Las Salas <jld03e@gmail.com>
2019-07-03 20:05:34 -0400

    updated handlebars version history to address dependency issues.

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,11 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE `burgers` (
-  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
-  `burger_name` VARCHAR( 255) NOT NULL,
-  `devoured` BOOLEAN,
+CREATE TABLE burgers (
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR( 255) NOT NULL,
+  devoured BOOLEAN,
+  createdAt TIMESTAMP NOT NULL,
 
-  PRIMARY KEY ( `id` ) 
+  PRIMARY KEY ( id ) 
 );
